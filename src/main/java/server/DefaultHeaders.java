cc8d594ab5c0f77991dec10f1db7d38b92986d8d
housenyao <housenyao@yzsj.net.cn>
2024-06-13 18:01:36 +0800

    引入HttpRequest和HttpResponse

---
+++ src/main/java/server/DefaultHeaders.java
@@ -0,0 +1,17 @@
+package server;
+
+/**
+ * @author kbws
+ * @date 2024/6/13
+ * @description: 常量类
+ */
+public class DefaultHeaders {
+
+    static final String HOST_NAME = "host";
+    static final String CONNECTION_NAME = "connection";
+    static final String ACCEPT_LANGUAGE_NAME = "accept-language";
+    static final String CONTENT_LENGTH_NAME = "content-length";
+    static final String CONTENT_TYPE_NAME = "content-type";
+    static final String TRANSFER_ENCODING_NAME = "transfer-encoding";
+
+}
---
+++ src/main/java/server/HttpResponse.java
@@ -0,0 +1,94 @@
+package server;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Locale;
+
+/**
+ * @author kbws
+ * @date 2024/6/13
+ * @description: Response返回对应的HTTP协议接口定义
+ */
+public class HttpResponse implements ServletResponse {
+    @Override
+    public String getCharacterEncoding() {
+        return null;
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public ServletOutputStream getOutputStream() throws IOException {
+        return null;
+    }
+
+    @Override
+    public PrintWriter getWriter() throws IOException {
+        return null;
+    }
+
+    @Override
+    public void setCharacterEncoding(String s) {
+
+    }
+
+    @Override
+    public void setContentLength(int i) {
+
+    }
+
+    @Override
+    public void setContentLengthLong(long l) {
+
+    }
+
+    @Override
+    public void setContentType(String s) {
+
+    }
+
+    @Override
+    public void setBufferSize(int i) {
+
+    }
+
+    @Override
+    public int getBufferSize() {
+        return 0;
+    }
+
+    @Override
+    public void flushBuffer() throws IOException {
+
+    }
+
+    @Override
+    public void resetBuffer() {
+
+    }
+
+    @Override
+    public boolean isCommitted() {
+        return false;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void setLocale(Locale locale) {
+
+    }
+
+    @Override
+    public Locale getLocale() {
+        return null;
+    }
+}
