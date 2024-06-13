3a74f9495a69dbe1c3952a0ad8ca6708f6f7db98
housenyao <housenyao@yzsj.net.cn>
2024-06-13 15:35:46 +0800

    引入池化技术以及Processor多线程

--- src/main/java/server/HttpConnector.java
+++ src/main/java/server/HttpConnector.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * @author kbws
@@ -11,6 +13,13 @@ import java.net.Socket;
  * @description: 负责处理接收、响应客户端请求
  */
 public class HttpConnector implements Runnable {
+
+    int minProcessors = 3;
+    int maxProcessors = 10;
+    int curProcessors = 0;
+
+    // 存放多个 processors 的池
+    Deque<HttpProcessor> processors = new ArrayDeque<>();
 
     @Override
     public void run() {
@@ -22,11 +31,18 @@ public class HttpConnector implements Runnable {
             e.printStackTrace();
             System.exit(1);
         }
+        // 初始化 processors 池
+        for (int i = 0; i < minProcessors; i++) {
+            HttpProcessor httpProcessor = new HttpProcessor(this);
+            processors.push(httpProcessor);
+        }
+        curProcessors = minProcessors;
         while (true) {
             Socket socket = null;
             try {
                 socket = serverSocket.accept();
-                HttpProcessor processor = new HttpProcessor();
+                // 从池中获取一个 processor（池中有可能新建）
+                HttpProcessor processor = createProcessor();
                 processor.process(socket);
                 // 关闭 socket 连接
                 socket.close();
@@ -36,8 +52,36 @@ public class HttpConnector implements Runnable {
         }
     }
 
+    // 从池子中获取一个processor，如果池子为空且小于最大限制，则新建一个
+    private HttpProcessor createProcessor() {
+        synchronized (processors) {
+            if (processors.size() > 0) {
+                //获取一个
+                return ((HttpProcessor) processors.pop());
+            }
+            if (curProcessors < maxProcessors) {
+                //新建一个
+                return (newProcessor());
+            } else {
+                return (null);
+            }
+        }
+    }
+
+    // 新建一个processor
+    private HttpProcessor newProcessor() {
+        HttpProcessor initprocessor = new HttpProcessor(this);
+        processors.push(initprocessor);
+        curProcessors++;
+        return ((HttpProcessor) processors.pop());
+    }
+
     public void start() {
         Thread thread = new Thread(this);
         thread.start();
+    }
+
+    void recycle(HttpProcessor processor) {
+        processors.push(processor);
     }
 }
--- src/main/java/server/HttpProcessor.java
+++ src/main/java/server/HttpProcessor.java
@@ -9,24 +9,38 @@ import java.net.Socket;
  * @date 2024/6/12
  * @description: 负责调用 Servlet
  */
-public class HttpProcessor {
-    public HttpProcessor() {
+public class HttpProcessor implements Runnable {
+
+    Socket socket;
+
+    boolean available = false;
+
+    HttpConnector httpConnector;
+
+    public HttpProcessor(HttpConnector httpConnector) {
+        this.httpConnector = httpConnector;
     }
 
     public void process(Socket socket) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
         InputStream input = null;
         OutputStream output = null;
-
         try {
             input = socket.getInputStream();
             output = socket.getOutputStream();
-            // 创建一个请求对象，并且处理
+            // 创建请求对象并解析
             Request request = new Request(input);
             request.parse();
-            // 创建一个响应对象
+            // 创建响应对象
             Response response = new Response(output);
             response.setRequest(request);
-            // 检查这是否是一个 Servlet 请求或者静态资源请求，一个 Servlet 请求应该以`/servlet/`开头
+            // response.sendStaticResource();
+            // 检查这是对servlet还是静态资源的请求
+            // a request for a servlet begins with "/servlet/"
             if (request.getUri().startsWith("/servlet/")) {
                 ServletProcessor processor = new ServletProcessor();
                 processor.process(request, response);
@@ -34,10 +48,65 @@ public class HttpProcessor {
                 StaticResourceProcessor processor = new StaticResourceProcessor();
                 processor.process(request, response);
             }
-            // 关闭连接
+            // 关闭 socket
             socket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    synchronized void assign(Socket socket) {
+        // 等待 connector 提供一个新的 socket
+        while (available) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+
+            }
+        }
+        // 获取到新的 socket
+        this.socket = socket;
+        // 把标志设置回去
+        available = true;
+        // 通知另外的线程
+        notifyAll();
+    }
+
+    private synchronized Socket await() {
+        // 等待 connector 提供一个新的 socket
+        while (!available) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+
+            }
+        }
+        // 获得这个新的Socket
+        Socket socket = this.socket;
+        //设置标志为false
+        available = false;
+        //通知另外的线程
+        notifyAll();
+        return (socket);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            // 等待 Socket 分配过来
+            Socket socket = await();
+            if (socket == null) {
+                continue;
+            }
+            // 处理请求
+            process(socket);
+            // 回收 processor
+            httpConnector.recycle(this);
+        }
+    }
+
+    public void start() {
+        Thread thread = new Thread(this);
+        thread.start();
     }
 }
